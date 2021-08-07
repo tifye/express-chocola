8ddcbb6f2edc938e07f34465991bac6a4a9b0ee1
tifufu <dematasjoshua@hotmail.com>
2021-08-07 08:22:51 +0200

    removed puesdo testing from project

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+import RouteGroup from './RouteGroup';
+import RouteRegistry from './RouteRegistry';
+import RouteTag from './RouteTag';
+
+export {
+  RouteGroup,
+  RouteRegistry,
+  RouteTag,
+};
+
 export * from './Route';
 export * from './RouteGroup';
 export * from './RouteRegistry';
